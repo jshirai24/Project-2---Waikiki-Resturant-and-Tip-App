9225842ad1ec7aa1c079ac6926f26c9fb1830eda
Jillian Shirai <jtshirai@hawaii.edu>
2026-04-09 12:08:29 -1000

    Initial Commit

---
+++ Project 2 - Waikiki Resturant and Tip App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2 - Waikiki Resturant and Tip App
+//
+//  Created by Jillian Shirai on 4/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
