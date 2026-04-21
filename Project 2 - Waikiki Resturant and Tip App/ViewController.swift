57b5cbe5032f6dca57678a0581f781c286fe3ef9
Jillian Shirai <jtshirai@hawaii.edu>
2026-04-20 19:21:55 -1000

    Started working on Project 2

--- Project 2 - Waikiki Resturant and Tip App/ViewController.swift
+++ Project 2 - Waikiki Resturant and Tip App/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var restaurantTableView: UITableView!
+    
+    @IBOutlet weak var waikikiMapView: MKMapView!
+    
+    var restaurantFoodImageData = [String]()
+    
+    var restaurantArray = ["Maguro Brothers ⭐️⭐️⭐️⭐️", "Marugame Udon ⭐️⭐️⭐️", "Eggs 'n Things ⭐️⭐️⭐️⭐️", "Liliha Bakery ⭐️⭐️⭐️⭐️", "Gyu-Kaku Japanese BBQ ⭐️⭐️⭐️", "Doraku Sushi ⭐️⭐️⭐️⭐️"]
+    
+    let centeredLocation = CLLocation(latitude: 21.279363, longitude: -157.829224)
+    let regionRadius: CLLocationDistance = 10000
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = restaurantArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centerMapOnLocation(location: centeredLocation)
+        
+        
+        
+        
+        
     }
-
-
+    func centerMapOnLocation (location: CLLocation)
+    {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        waikikiMapView.setRegion(coordinateRegion, animated: true)
+    }
+    
 }
-
---
+++ Project 2 - Waikiki Resturant and Tip App/restaurantObjectOne.swift
@@ -0,0 +1,29 @@
+//
+//  restaurantObjectOne.swift
+//  Project 2 - Waikiki Resturant and Tip App
+//
+//  Created by Jillian Shirai on 4/20/26.
+//
+
+import UIKit
+import MapKit
+
+class restaurantObjectOne: NSObject, MKAnnotation {
+    let resturantTitle: String?
+    let coordinate: CLLocationCoordinate2D
+    init(title: String, coordinate: CLLocationCoordinate2D )
+    {
+        self.resturantTitle = title
+        self.coordinate = coordinate
+        
+        super.init()
+        
+        
+    }
+    
+    var subtitle: String?
+    {
+        return resturantTitle
+    }
+
+}
