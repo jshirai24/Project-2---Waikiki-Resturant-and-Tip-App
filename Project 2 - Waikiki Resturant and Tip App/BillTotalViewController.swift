fac96718483c49f9fa9cec99d3128e5b7661e4ce
Jillian Shirai <jtshirai@hawaii.edu>
2026-04-22 20:03:32 -1000

    Completed tab 2

--- Project 2 - Waikiki Resturant and Tip App/BillTotalViewController.swift
+++ Project 2 - Waikiki Resturant and Tip App/BillTotalViewController.swift
@@ -36,7 +36,48 @@ class BillTotalViewController: UIViewController {
         billTotalTextField.placeholder = "Enter the Bill amount"
         tipPercentageControl.selectedSegmentIndex = -1
         titleLabel.text = "Guide to Tipping"
+        tipDescriptLabel.text = "Tipping is something that is expected when dining in the USA. As those in the service industry often rely on these tips, it is recommended and encouraged to tip after a meal. 15% of your bill is the average baseline for tipping for service. However, for great service the amount can increase to 18-20%."
+        setPercentageLabel.text = "Set Tip%"
+        tipAmountLabel.text = "Your Tip Amount:"
+        totalBillAndTipAmountLabel.text = "Your Total Bill + Tip Amount:"
+        tipResultLabel.text = ""
+        billLabel.text = ""
         
+    }
+    
+
+    @IBAction func tipPercentagePressed(_ sender: Any)
+    {
+        switch tipPercentageControl.selectedSegmentIndex
+        {
+           
+        case 0:
+            let billAmount = Float((billTotalTextField.text!))!
+            let tipAmountCalculation = 0.15 * billAmount
+            let totalAmount = billAmount + tipAmountCalculation
+            tipResultLabel.text = "\(tipAmountCalculation)"
+            billLabel.text = "\(totalAmount)"
+        
+        case 1:
+            let billAmount = Float((billTotalTextField.text!))!
+            let tipAmountCalculation = 0.18 * billAmount
+            let totalAmount = billAmount + tipAmountCalculation
+            tipResultLabel.text = "\(tipAmountCalculation)"
+            billLabel.text = "\(totalAmount)"
+            
+            
+        case 2:
+            let billAmount = Float((billTotalTextField.text!))!
+            let tipAmountCalculation = 0.20 * billAmount
+            let totalAmount = billAmount + tipAmountCalculation
+            tipResultLabel.text = "\(tipAmountCalculation)"
+            billLabel.text = "\(totalAmount)"
+            
+        default:
+            break
+        
+            
+        }
         
         
         
@@ -45,7 +86,11 @@ class BillTotalViewController: UIViewController {
         
     }
     
-
+    
+    
+    
+    
+    
     
 
 }
